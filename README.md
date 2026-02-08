c1a1a2de19c8f8deed07eae8952a62d43d38b831
Anthony <axnguyen@umich.edu>
2026-02-08 16:31:40 -0500

    Set up camera, added to README, and added a .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SURE-Project-9-Task-Guidance-System-for-Table-Organization
+A full pipeline that perceives the real world, tracks the state of a physical workflow, and provides visual feedback to guide a user.
+
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/vin/activate
+
+# Install Dependicies
+pip install opencv-python numpy
+
+
+# Exiting the Program 
+Click on the OpenCV window to give it focus
+Press the q key to quit
---
+++ camera_test.py
@@ -0,0 +1,26 @@
+import cv2
+
+video_capture = cv2.VideoCapture(0)  # 0 = default webcam
+
+if not video_capture.isOpened():
+    print("Error: Could not open camera")
+    exit()
+
+while True:
+    success, frame = video_capture.read()
+    if not success:
+        print("Failed to grab frame")
+        break
+
+    # Show frame
+    cv2.imshow("Webcam Test", frame)
+
+    # Press 'q' to quit
+    # cv2.waitKey waits 1 millisecond and checks for keyboard input 
+    # waitKey returns -1 -> no key pressed
+    # or waitKey returns key pressed (ASCII)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release() # Closes Webcam 
+cv2.destroyAllWindows() # Closes any OpenCV-created windows
